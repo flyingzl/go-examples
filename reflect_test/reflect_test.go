46862e40cab659bf21cbc7ced7c4b9adaf218712
zhaolei <zhaolei@migu.cn>
2020-06-09 14:27:07 +0800

    feat: 完善reflect样例

--- reflect_test/reflect_test.go
+++ reflect_test/reflect_test.go
@@ -10,7 +10,7 @@ type Student struct {
 	Name string `json:"userName"`
 }
 
-func (s Student) Greet(msg string) {
+func (s *Student) Greet(msg string) {
 	fmt.Printf("%s say: %s\n", s.Name, msg)
 }
 
@@ -20,11 +20,13 @@ func detect(v interface{}) {
 	retValue := reflect.ValueOf(v)
 	fmt.Println(retType, retValue)
 
+	// 检测是否 为 指针
 	if retType.Kind() == reflect.Ptr {
 		if field, ok := retType.Elem().FieldByName("Name"); ok {
 			fmt.Println(field.Tag.Get("json"))
 		}
 
+		// 检测是否允许修改
 		nameVal := retValue.Elem().FieldByName("Name")
 		if nameVal.CanSet() {
 			nameVal.SetString("Jim")
@@ -41,7 +43,11 @@ func detect(v interface{}) {
 	}
 
 	params := []reflect.Value{reflect.ValueOf("hello")}
-	reflect.ValueOf(v).MethodByName("Greet").Call(params)
+	fnVal := retValue.MethodByName("Greet")
+	// Greet方法只能在使用指针时才调用
+	if fnVal.IsValid() {
+		fnVal.Call(params)
+	}
 }
 
 func TestReflect(t *testing.T) {
